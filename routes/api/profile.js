d4a1a04c6d933ad10f83846fedca7738969f505e
Steven Bunde <stevenbunde2020@gmail.com>
2023-10-24 19:16:30 -0400

    added path to delete an experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,5 +211,27 @@ router.put('/experience',
     }
 );
 
+// @route:   DELETE api/profile/experience/:exp_id
+// @desc:    Delete experience from profile
+// @access:  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get the index of the experience to remove
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        // Remove the experience using .splice() which takes in the index of the item to remove
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
